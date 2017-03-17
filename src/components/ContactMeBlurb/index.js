b0e92610e93fe0240dfc677750d7f6597ae48181
Samuel Jackson <samuelhkjackson@gmail.com>
2017-03-17 10:17:49 -0700

    Adding GitHub pages and updating my resume link.

--- src/components/ContactMeBlurb/index.js
+++ src/components/ContactMeBlurb/index.js
@@ -3,7 +3,7 @@ import './index.css';
 
 // ContactMeBlurb renders the text paragraph and link to resume
 function ContactMeBlurb() {
-  const resumeLink = 'https://www.dropbox.com/s/sac7hq627uxq792/Samuel%20Jackson%20Resume.pdf?dl=0';
+  const resumeLink = 'https://www.dropbox.com/s/5uhcyx8fvg57632/Samuel%20Jackson%20-%20Resume%20-%202017.pdf?dl=0';
   const noLinkContent = ' (you’ll find my email and phone number inside). Click below for my GitHub, LinkedIn, or Twitter.';
 
   const resumeElement = (
